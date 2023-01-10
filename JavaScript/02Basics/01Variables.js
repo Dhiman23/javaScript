a9f7e45fd8076b5f069013f26ea7804314561ac5
sajal dhiman <92822169+Dhiman23@users.noreply.github.com>
2023-01-10 07:50:13 +0000

    added

---
+++ JavaScript/02Basics/01Variables.js
@@ -0,0 +1,13 @@
+var fullName = "sajal Dhiman";
+
+var courseName = "React JS BootCamp";
+
+var isLoggedIn = false;
+
+var loggedCount = 34;
+console.log(loggedCount);
+console.log("loggedCount");
+
+var paymentMode;
+
+paymentMode = "Credit card";
---
+++ JavaScript/02Basics/02Variables-two.js
@@ -0,0 +1,48 @@
+// const uid = "abc123";
+
+// var fullName = "sajal dhiman";
+// var email = "sajaldhiman16@gmail.com";
+// var password = "1234";
+// var confirmPassword = "1234";
+// var courseCount = 0;
+// var isLoggedInFromGoogle = false;
+
+// // prompt("Enter your name");
+
+// console.log(uid);
+// console.log("full name is :",fullName);
+// console.log(email);
+
+
+// console.log(`
+//  with Unique ID: ${uid}
+//  User is : ${fullName}
+//  and his email is : ${email}
+//  and Uses the password : ${password}
+
+// `);
+
+// -----------------------------------------------------------------------------
+
+var firstName = "Sajal";
+var lastName = "Dhiman";
+var countryName = "India";
+var courseCount = 0;
+var isLoggedInFromGoogle = false;
+var isLoggedInFromFacebook = false;
+const uniqueId = "123456";
+var password = "Qwert123";
+var confirmPassword = "Qwert123";
+var email = "Sajaldhiman68@gmail.com";
+
+
+console.log(`
+User first name : ${firstName}
+User last name : ${lastName}
+Country Name : ${countryName}
+Google Login : ${isLoggedInFromGoogle}
+Facebook Login : ${isLoggedInFromFacebook}
+User id : ${uniqueId}
+User name : ${email}
+`);
+ 
