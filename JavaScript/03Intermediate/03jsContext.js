446e1ffc402d775cf3a91ac1a0b66860bec92c83
Dhiman23 <sajaldhiman68@gmail.com>
2023-01-13 01:20:07 +0530

    pushed

---
+++ JavaScript/03Intermediate/03jsContext.js
@@ -0,0 +1,15 @@
+// sayHello();
+
+// function sayHello(){
+//     console.log("Hello");
+// }
+//(===) means we are checking the type as well as is it true or not 
+if(2==="2"){
+  console.log("This is true")
+}
+
+var myName = "sajal";
+if(myName === myName){
+    console.log("Thsi again a true statement");
+}
+
---
+++ JavaScript/03Intermediate/04Hosting.js
@@ -0,0 +1,32 @@
+// var num = 2;
+// function sayMe(){
+//     console.log("Say me");
+// }
+
+// sayMe();
+
+
+// function tipper(a){
+//     var bill = parseInt(a);
+//     console.log(bill + 5);
+// }
+
+// tipper(80);
+
+
+var bigTipper = function (a){
+    var bill = parseInt(a);
+    console.log(bill + 15);
+}
+
+bigTipper(200);
+
+var name1 = "sajal";
+console.log(name1);
+
+function sayName(){
+    var name2 = "MR. S";
+    console.log(name2);
+}
+
+sayName();
